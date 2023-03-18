67bae22446ec779d395e99d92b55e70146bc8f01
Otavio Maya <82895172+knz13@users.noreply.github.com>
2023-03-17 22:11:29 -0300

    [fix] changing create library script
    # Description
    
    Fixed an issue with the create library script preventing the build on windows

--- create_library_and_add_to_assets.py
+++ create_library_and_add_to_assets.py
@@ -17,7 +17,14 @@ os.system('echo "Creating c++ library..."')
 os.system(f'cmake {"-DCMAKE_BUILD_TYPE=" + ("Release" if args.Release else "Debug") if platform.system() == "Darwin" else ""} -B build ./')
 os.system('echo "CMake run finished!')
 os.system('echo "Compiling..."')
-os.system(f'cd build && {"make" if platform.system() == "Darwin" else "msbuild project_typhon /p:Configuration=" + "Release" if args.Release else "Debug"}')
+os.chdir("build")
+os.system(f'{"make" if platform.system() == "Darwin" else "msbuild project_typhon.sln /p:Configuration=" + ("Release" if args.Release else "Debug")}')
 os.system('echo "Moving library to assets..."')
-os.system(f'cd build && {"mv libtyphon.dylib ../../assets/lib/libtyphon.dylib" if platform.system() == "Darwin" else "MOVE "}')
+
+if platform.system() == "Darwin":
+    os.rename(os.path.abspath("libtyphon.dylib"),os.path.abspath("../../assets/lib/libtyphon.dylib"))
+else:
+    os.rename(os.path.abspath("Debug/typhon.dll" if not args.Release else "Release/typhon.dll"),os.path.abspath("../../assets/lib/typhon.dll"))
+    
+
 os.system('echo "Build finished!"')
