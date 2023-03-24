d4bd1e2648e4271f615a6daf30a9b25ce30a3ad0
Otavio Maya <82895172+knz13@users.noreply.github.com>
2023-03-24 20:03:51 -0300

    fixing glslc

--- create_library_and_add_to_assets.py
+++ create_library_and_add_to_assets.py
@@ -13,22 +13,38 @@ parser.add_argument("--Release",action='store_true')
 
 args = parser.parse_args()
 
+current_dir = os.path.abspath(os.curdir)
 os.chdir("c_sharp_interface")
 if not os.path.exists("vendor"):
     os.mkdir("vendor")
 
 if not os.path.exists("vendor/shaderc"):
     os.mkdir("vendor/shaderc")
+if not os.path.exists(os.path.join(current_dir,f"/assets/lib/{'libshaderc.dll' if platform.system() != 'Darwin' else 'liblibshaderc.dylib'}")):
+    if not os.path.exists("vendor/shaderc/utils"):
+        os.system('echo "Building spirv compiler library..."')
+        os.system("git clone https://github.com/google/shaderc vendor/shaderc")
+        os.system("vendor/shaderc/utils/git-sync-deps")
+        os.system("cmake -B build/shaderc_compiler vendor/shaderc")
+    if not os.path.exists("build/shaderc_compiler/glslc"):
+        os.chdir("build/shaderc_compiler/glslc")
+        os.system(f'{"make" if platform.system() == "Darwin" else "msbuild project_typhon.sln /p:Configuration=" + ("Release" if args.Release else "Debug")}')
+        os.chdir("../../../")
+    os.chdir("build/shaderc_compiler/glslc")
+    if platform.system() == "Darwin":
+        if os.path.exists(os.path.abspath(os.path.join(current_dir,"assets/lib/libglslc.a"))):
+            os.remove(os.path.abspath(os.path.join(current_dir,"assets/lib/libglslc.a")))
+            
+        os.rename(os.path.abspath("libglslc.a"),os.path.abspath(os.path.join(current_dir,"assets/lib/libglslc.a")))
+    else:
+        #TODO
+        quit()
+        if os.path.exists(os.path.abspath("../../assets/lib/typhon.dll")):
+            os.remove(os.path.abspath("../../assets/lib/typhon.dll"))
+        os.rename(os.path.abspath("Debug/typhon.dll" if not args.Release else "Release/typhon.dll"),os.path.abspath("../../assets/lib/typhon.dll"))
+        
 
-if not os.path.exists(f"../assets/lib/{'libshaderc.dll' if platform.system() != 'Darwin' else 'liblibshaderc.dylib'}"):
-    os.system('echo "Building spirv compiler library..."')
-    os.system("git clone https://github.com/google/shaderc vendor/shaderc")
-    os.system("vendor/shaderc/utils/git-sync-deps")
-    os.system("cmake -B build/shaderc_compiler vendor/shaderc")
-    os.chdir("build/shaderc_compiler/libshaderc")
-    os.system(f'{"make" if platform.system() == "Darwin" else "msbuild project_typhon.sln /p:Configuration=" + ("Release" if args.Release else "Debug")}')
-    os.chdir("../../")
-
+    os.chdir(os.path.join(current_dir,"c_sharp_interface"))
 
 os.system('echo "Creating c++ library..."')
 os.system(f'cmake {"-DCMAKE_BUILD_TYPE=" + ("Release" if args.Release else "Debug") if platform.system() == "Darwin" else ""} -B build ./')
